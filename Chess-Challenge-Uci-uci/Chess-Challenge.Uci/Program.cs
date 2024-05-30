69b0d1ba14e2f8709f294468a2a14e2f174829d9
Dragjon <140328303+Dragjon@users.noreply.github.com>
2024-05-31 00:27:40 +0800

    division by zero error fix

---
+++ Chess-Challenge-Uci-uci/Chess-Challenge.Uci/Program.cs
@@ -0,0 +1,22 @@
+﻿using ChessChallenge.Application;
+
+namespace Chess_Challenge.Cli
+{
+    internal class Program
+    {
+        static (int, int) GetTokenCount()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "src", "My Bot", "MyBot.cs");
+            string txt = File.ReadAllText(path);
+            return TokenCounter.CountTokens(txt);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sebastian Lague's Chess Challenge UCI interface by Gediminas Masaitis");
+
+            var uci = new Uci();
+            uci.Run();
+        }
+    }
+}
