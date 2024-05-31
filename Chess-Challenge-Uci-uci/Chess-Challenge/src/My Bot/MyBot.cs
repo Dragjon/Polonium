f9b4b550087ce5bf58d1fbaa102297e19fe1ad8a
Dragjon <140328303+Dragjon@users.noreply.github.com>
2024-06-01 00:26:14 +0800

    v0.0.8 Principle Variation Search (8.84 +/- 5.98)
    --------------------------------------------------
    Results of PVS vs Original:
    Elo: 8.84 +/- 5.98, nElo: 10.90 +/- 7.37
    LOS: 99.81 %, DrawRatio: 41.22 %, PairsRatio: 1.16
    Games: 8534, Wins: 3319, Losses: 3102, Draws: 2113, Points: 4375.5 (51.27 %)
    Ptnml(0-2): [501, 661, 1759, 812, 534]
    LLR: 2.97 (-2.94, 2.94) [0.00, 5.00]
    --------------------------------------------------

--- Chess-Challenge-Uci-uci/Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge-Uci-uci/Chess-Challenge/src/My Bot/MyBot.cs
@@ -283,6 +283,7 @@ public class MyBot : IChessBot
             int max = -infinity;
             ulong hash = board.ZobristKey % 33554432;
             bool isRoot = ply == 0;
+            bool pvNode = beta - alpha != 1;
             bool nodeIsCheck = board.IsInCheck();
             Move[] legals = board.GetLegalMoves();
 
@@ -297,7 +298,7 @@ public class MyBot : IChessBot
 
             int eval = Eval(board);
 
-            if (eval > beta && board.TrySkipTurn())
+            if (eval > beta && !pvNode && board.TrySkipTurn())
             {
                 int nullScore = -AlphaBeta(depth - nullMoveR - 1, ply + 1, -beta, -beta + 1);
                 board.UndoSkipTurn();
@@ -307,12 +308,27 @@ public class MyBot : IChessBot
                 }
             }
 
+            int moveCount = 0;
 
             foreach (Move move in legals.OrderByDescending(move => move == TT[hash] ? 1_000_000_000 : move.IsCapture ? mvvlvaTable[(int)move.MovePieceType - 1, (int)move.CapturePieceType - 1] : move == killers[ply] ? 1_000_000 : history[move.StartSquare.Index, move.TargetSquare.Index]))
             {
                 nodes++;
+                moveCount++;
                 board.MakeMove(move);
-                int score = -AlphaBeta(depth - 1, ply + 1, -beta, -alpha);
+                int score = 0;
+                if (moveCount == 1 && pvNode)
+                {
+                    score = -AlphaBeta(depth - 1, ply + 1, -beta, -alpha);
+                }
+                else
+                {
+                    score = -AlphaBeta(depth - 1, ply + 1, -alpha - 1, -alpha);
+
+                    if (beta > score && score > alpha)
+                    {
+                        score = -AlphaBeta(depth - 1, ply + 1, -beta, -alpha);
+                    }
+                }
                 board.UndoMove(move);
 
                 if (score > alpha)
